9ab26eaa222a3e4143f3d70aa70627a1101b692e
Juan Pablo Pieczocha <70308282+JPPieczocha@users.noreply.github.com>
2022-04-30 16:23:10 -0300

    REST Controller

---
+++ src/main/java/edu/uade/arqapp/blasphemyfilter/controllers/MessageController.java
@@ -0,0 +1,26 @@
+package edu.uade.arqapp.blasphemyfilter.controllers;
+
+import edu.uade.arqapp.blasphemyfilter.domain.MessageRequest;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/messages")
+public class MessageController {
+    
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public MessageController(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping
+    public void publish(@RequestBody MessageRequest request) {
+        kafkaTemplate.send("output", request.message());
+    }
+
+}
